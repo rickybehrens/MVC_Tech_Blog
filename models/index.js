daade39a2c6454975eb2861b8aa828be7a86e588
Ricardo Behrens <behrensricardo@gmail.com>
2023-09-30 13:42:24 -0600

    Added the models

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comments = require('./Comments');
+
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Comments.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Comments.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Comments, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Comments, {
+    foreignKey: 'post_id'
+});
+
+module.exports = {User, Post, Comments};
--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
@@ -12,8 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
+  secret: process.env.SECRET,
+  cookie: {
+    // Session will automatically expire in 5 minutes
+    expires: 5 * 60 * 1000
+},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
