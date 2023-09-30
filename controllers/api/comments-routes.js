a28b37ba338dc0efbeb2d3af3abb47951b6f12d3
Ricardo Behrens <behrensricardo@gmail.com>
2023-09-30 14:07:38 -0600

    Working on my routes but failing miserably...

---
+++ controllers/api/comments-routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { Comments } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Comments.findAll({})
+      .then(dbCommentsData => res.json(dbCommentsData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+router.post('/', withAuth, (req, res) => {
+  // check the session
+  if (req.session) {
+    Comments.create({
+      Comments_text: req.body.Comments_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id,
+    })
+      .then(dbCommentsData => res.json(dbCommentsData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comments.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+        .then(dbCommentsData => {
+          if (!dbCommentsData) {
+            res.status(404).json({ message: 'No Comments found with this id' });
+            return;
+          }
+          res.json(dbCommentsData);
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+});
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const commentsRoutes = require('./comments-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/comments', commentsRoutes);
 
 module.exports = router;
