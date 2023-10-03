26beac52c078a957d0931a40af9f87afc42eda76
Ricardo Behrens <behrensricardo@gmail.com>
2023-10-03 15:31:11 -0600

    I was able to finally seed the tables. I was trying to seed everything from one file but the moment I did one for user and another for post it worked. I still cant see the dashboard and the posts, but I can see them in MySQL Workbench

--- seeds/index.js
+++ seeds/index.js
@@ -1,14 +1,25 @@
-const seedPosts = require('./blogsData');
-
 const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
 
-const seedAll = async () => {
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedPosts();
-  console.log('\n----- POSTS SEEDED -----\n');
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
 
-seedAll();
+seedDatabase();
