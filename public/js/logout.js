5d24c91e75737ec32faba94bdc75dea013eda47b
Ricardo Behrens <behrensricardo@gmail.com>
2023-10-10 17:50:40 -0600

    last small changes

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,19 +1,16 @@
 const logout = async function () {
-  console.log("maybe this will log me out?");
-  
-    const response = await fetch('/api/users/logout', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/login');
-      console.log('loggin out too');
-    } else {
-      alert(response.statusText);
-    }
-  }
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' }
+  });
 
-  
+  if (response.ok) {
+    document.location.replace('/login');
+    console.log('loggin out too');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('#logout').addEventListener('click', logout);
+
